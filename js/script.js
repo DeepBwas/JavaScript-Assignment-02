a86185fbe12804e387e89a3c25db32dcdce6c48c
DeepBiswas <105808324+DeepBwas@users.noreply.github.com>
2024-02-10 20:38:35 -0500

    Added color change

--- js/script.js
+++ js/script.js
@@ -1,8 +1,36 @@
 // constants for query selector
-
+const studentId = document.querySelector("#myStudentId");
+const customNumber = document.querySelector("#customNumber");
+const customColor = document.querySelector(".custColor");
+const randomColor = document.querySelector(".randColor");
+const selectImage = document.querySelector("#imageSelect");
+const image = document.querySelector("#images");
 // function to change bg color from user input and add student id
-function changeCustomColor() {
-
+function changeCustomColor(){
+    let theValue = customNumber.value;
+    if (theValue < 0 || theValue > 100){
+        document.body.style.backgroundColor = "red";
+    } else {
+        switch (true) {
+            case (theValue >= 0 && theValue <= 20):
+                document.body.style.backgroundColor = "green";
+                break;
+            case (theValue > 20 && theValue <= 40):
+                document.body.style.backgroundColor = "blue";
+                break;
+            case (theValue > 40 && theValue <= 60):
+                document.body.style.backgroundColor = "orange";
+                break;
+            case (theValue > 60 && theValue <= 80):
+                document.body.style.backgroundColor = "purple";
+                break;
+            case (theValue > 80 && theValue <= 100):
+                document.body.style.backgroundColor = "yellow";
+                break;
+            default:
+                document.body.style.backgroundColor = "white";
+        }
+    }
 }
 
 // function to change bg color from random no.
@@ -22,5 +50,6 @@ function changeImage() {
 }
 
 // event listeners for on click event of buttons and select
+customColor.addEventListener("click", changeCustomColor);
 
 // event listeners for on change event of select
